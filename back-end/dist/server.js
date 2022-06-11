0f0e56094146f7f30f4e25588993b4cacc14e41d
Elinaldo1 <elinnaldosc@gmail.com>
2022-06-11 09:57:06 -0300

    adjust in env.PORT file dist

--- back-end/dist/server.js
+++ back-end/dist/server.js
@@ -10,4 +10,4 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(routes_1.routes);
-app.listen(3333, () => console.log('api online'));
+app.listen(process.env.PORT || 3333, () => console.log('api online'));
